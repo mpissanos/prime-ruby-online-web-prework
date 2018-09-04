b7522fd4376a65ada67249fc8feb27fdb95dce1c
Manny Pissanos <mpissano@gmail.com>
2018-09-04 20:08:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(n)
-  return false if integer < 2
-  (2..integer - 1).each do |x|
-    if (integer % x) == 0
+  return false if n < 2
+  (2..n - 1).each do |x|
+    if (n % x) == 0
       return false
     end
   end
