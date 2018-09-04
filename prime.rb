f5ed9b7b6a6bdb7ca9ac9e3756cbfa92fa764d50
Manny Pissanos <mpissano@gmail.com>
2018-09-04 20:07:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,10 @@
 def prime?(n)
-  divisor = 2
-  while n > divisor
-    n % divisor == 0
-    return false
-  else
-    divisor ++
-  end
-    return true
-end
-
-
-def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
-            return false
-        end
+  return false if integer < 2
+  (2..integer - 1).each do |x|
+    if (integer % x) == 0
+      return false
     end
-    true
+  end
+  true
 end
+
