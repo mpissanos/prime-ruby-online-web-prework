b99cb20fd9d026464d04a5c3631850a8f0a1fb25
Manny Pissanos <mpissano@gmail.com>
2018-09-04 20:05:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
-# def prime?(n)
-#   divisor = 2
-#   while n > divisor
-#     n % divisor == 0
-#     return false
-#   else
-#     divisor ++
-#   end
-#     return true
-# end
+def prime?(n)
+  divisor = 2
+  while n > divisor
+    n % divisor == 0
+    return false
+  else
+    divisor ++
+  end
+    return true
+end
 
 
 def prime?(integer)
